11af646b1355f8afeb280d67f3853b52f10ecdad
Kai Arne Myklebust <kaimyk@Kais-MacBook-Pro.local>
2018-09-09 10:53:10 +0200

    Added some2 evalFull

--- Assignment 1/src/Arithmetic.hs
+++ Assignment 1/src/Arithmetic.hs
@@ -20,27 +20,43 @@ import Definitions
 
 showExp :: Exp -> String
 showExp (Cst as) = show as
-showExp (Add a b) = "(" ++ (showExp (a)) ++ " + " ++ (showExp (b)) ++ ")"
-showExp (Sub a b) = "(" ++ (showExp (a)) ++ " - " ++ (showExp (b)) ++ ")"
-showExp (Mul a b) = "(" ++ (showExp (a)) ++ " * " ++ (showExp (b)) ++ ")"
-showExp (Div a b) = "(" ++ (showExp (a)) ++ " / " ++ (showExp (b)) ++ ")"
-showExp (Pow a b) = "(" ++ (showExp (a)) ++ "^" ++ (showExp (b)) ++ ")"
+showExp (Add a b) = "(" ++ showExp a ++ " + " ++ showExp b ++ ")"
+showExp (Sub a b) = "(" ++ showExp a ++ " - " ++ showExp b ++ ")"
+showExp (Mul a b) = "(" ++ showExp a ++ " * " ++ showExp b ++ ")"
+showExp (Div a b) = "(" ++ showExp a ++ " / " ++ showExp b ++ ")"
+showExp (Pow a b) = "(" ++ showExp a ++ "^" ++ showExp b ++ ")"
 showExp _ = error "is not supported"
 
 evalSimple :: Exp -> Integer
 evalSimple (Cst a) = a
-evalSimple (Add a b) = evalSimple(a) + evalSimple(b)
-evalSimple (Sub a b) = evalSimple(a) - evalSimple(b)
-evalSimple (Mul a b) = evalSimple(a) * evalSimple(b)
-evalSimple (Div a b) = evalSimple(a) `div` evalSimple(b)
-evalSimple (Pow a b) = evalSimple(a) ^ evalSimple(b)
+evalSimple (Add a b) = evalSimple a + evalSimple b
+evalSimple (Sub a b) = evalSimple a - evalSimple b
+evalSimple (Mul a b) = evalSimple a * evalSimple b
+evalSimple (Div a b) = evalSimple a `div` evalSimple b
+evalSimple (Pow a b) = evalSimple a ^ evalSimple b
 evalSimple _ = error "is not supported"
 
 extendEnv :: VName -> Integer -> Env -> Env
 extendEnv v n r =  \a -> if v == a then Just n else r a
 
+intTest :: Maybe Integer -> Integer
+intTest (Just i) = i
+intTest _ = error "Value is unbound"
+
 evalFull :: Exp -> Env -> Integer
-evalFull = undefined
+evalFull (Cst a) _ = a
+evalFull (Add a b) r = evalFull a r + evalFull b r
+evalFull (Sub a b) r = evalFull a r - evalFull b r
+evalFull (Mul a b) r = evalFull a r * evalFull b r
+evalFull (Div a b) r = evalFull a r `div` evalFull b r
+evalFull (Pow a b) r = evalFull a r ^ evalFull b r
+
+evalFull (If _ _ _) _ = undefined
+evalFull (Var v) r = intTest(r v)
+evalFull (Let _ _ _) _ = undefined
+evalFull (Sum _ _ _ _) _ = undefined
+evalFull _ _ = error "is not supported"
+--evalFull (Var v) r = if r v == Nothing then error "HHH" else r v --Maybe integer
 
 evalErr :: Exp -> Env -> Either ArithError Integer
 evalErr = undefined
