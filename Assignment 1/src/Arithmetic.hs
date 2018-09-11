fe760307eab3c3f5053ce479fb20f1f65db4af4f
Kai Arne Myklebust <kaimyk@Kais-MacBook-Pro.local>
2018-09-11 11:26:30 +0200

    Slides from 2017 and let

--- Assignment 1/src/Arithmetic.hs
+++ Assignment 1/src/Arithmetic.hs
@@ -19,7 +19,7 @@ where
 import Definitions
 
 showExp :: Exp -> String
-showExp (Cst as) = show as
+showExp (Cst as) = if head(show as) == '-' then "(" ++ show as ++ ")" else show as
 showExp (Add a b) = "(" ++ showExp a ++ " + " ++ showExp b ++ ")"
 showExp (Sub a b) = "(" ++ showExp a ++ " - " ++ showExp b ++ ")"
 showExp (Mul a b) = "(" ++ showExp a ++ " * " ++ showExp b ++ ")"
@@ -33,15 +33,22 @@ evalSimple (Add a b) = evalSimple a + evalSimple b
 evalSimple (Sub a b) = evalSimple a - evalSimple b
 evalSimple (Mul a b) = evalSimple a * evalSimple b
 evalSimple (Div a b) = evalSimple a `div` evalSimple b
-evalSimple (Pow a b) = evalSimple a ^ evalSimple b
+evalSimple (Pow a b)
+  | evalSimple b < 0 = error "Negative exponent"
+  | evalSimple b == 0 = 1 
+  | otherwise = evalSimple a * evalSimple(Pow a (Sub b (Cst 1)))
 evalSimple _ = error "is not supported"
+-- evalSimple (Pow a b) = evalSimple a ^ evalSimple b
 
 extendEnv :: VName -> Integer -> Env -> Env
-extendEnv v n r =  \a -> if v == a then Just n else r a
+extendEnv v n r a = if v == a then Just n else r a
 
 intTest :: Maybe Integer -> Integer
 intTest (Just i) = i
 intTest _ = error "Value is unbound"
+
+summ :: VName -> Integer -> Integer -> Integer -> Env -> Integer
+summ v a b c r = if intTest(r v) > a then c else summ v a b (b+b) (extendEnv v (intTest(r v)+1) r)
 
 evalFull :: Exp -> Env -> Integer
 evalFull (Cst a) _ = a
@@ -53,10 +60,15 @@ evalFull (Pow a b) r = evalFull a r ^ evalFull b r
 
 evalFull (If a b c) r = if evalFull a r /= 0 then evalFull b r else evalFull c r
 evalFull (Var v) r = intTest(r v)
-evalFull (Let _ _ _) _ = undefined
-evalFull (Sum _ _ _ _) _ = undefined
-evalFull _ _ = error "is not supported"
---evalFull (Var v) r = if r v == Nothing then error "HHH" else r v --Maybe integer
+evalFull (Let a b c) r = evalFull c (extendEnv a (evalFull b r) r)
+evalFull (Sum v a b c) r = summ v (evalFull b r) (evalFull c r) 0 (extendEnv v (evalFull a r) r)
+
+-- if intTest(b r) > (evalFull c r) then 0 else
+-- extendEnv a ((evalFull b r) + 1) r (evalFull d r + evalFull (Sum a ))
+-- (evalFull d r + (evalFull (Sum a (b+1) c d) r))
+
+-- evalFull _ _ = error "is not supported"
+-- evalFull (Var v) r = if r v == Nothing then error "HHH" else r v --Maybe integer
 
 evalErr :: Exp -> Env -> Either ArithError Integer
 evalErr = undefined
