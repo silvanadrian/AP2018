f6f344b2869e1aa869d302c6722722d8eae40dc5
Kai Arne Myklebust <kai25arne@icloud.com>
2018-09-30 14:37:27 +0200

    select to select1

--- Assignment 3/src/twitbook.pl
+++ Assignment 3/src/twitbook.pl
@@ -29,10 +29,10 @@ dislikes(G, X, Y) :-
 
 /* different succeeds whenever X and Y are different members of the network G */
 different(G, X, Y) :-
-    select(person(X, _), G, G2),
-    select(person(Y, _), G2, _).
+    select1(person(X, _), G, G2),
+    select1(person(Y, _), G2, _).
 
 
-select(X, [X|Tail], Tail).
-select(Elem, [Head|Tail], [Head|Rest]) :-
-    select(Elem, Tail, Rest).
+select1(X, [X|Tail], Tail).
+select1(Elem, [Head|Tail], [Head|Rest]) :-
+    select1(Elem, Tail, Rest).
