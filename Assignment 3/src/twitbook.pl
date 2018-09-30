11187db8e133cbca2a9095c60f26932664e54ff6
Kai Arne Myklebust <kai25arne@icloud.com>
2018-09-30 14:24:02 +0200

    dislikes

--- Assignment 3/src/twitbook.pl
+++ Assignment 3/src/twitbook.pl
@@ -11,11 +11,20 @@ g1([person(kara, [barry, clark]),
  person(clark, [oliver, kara]),
  person(oliver, [kara])]).
 
-
+% a
 likes(G,X,Y) :- 
-	isMember(person(X, Friends), G), 
-	isMember(Y, Friends).
+    isMember(person(X, Friends), G),
+    isMember(Y, Friends).
 
 /* Checks if an elem is member of list */
 isMember(Head,[Head|_]).
 isMember(Head,[_|Tail]) :- isMember(Head,Tail).
+
+% b - using not which may not be allowed
+% different also not in use
+dislikes(G, X, Y) :-
+    % different(G, X, Y),
+    likes(G, Y, X),
+    not(likes(G, X, Y)).
+    
+% different(G, X, Y) :-
