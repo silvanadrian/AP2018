5a9ed9dd65880445f72b86e418329f552c11c43b
Kai Arne Myklebust <kai25arne@icloud.com>
2018-10-02 03:01:38 +0200

    admires

--- Assignment 3/src/twitbook.pl
+++ Assignment 3/src/twitbook.pl
@@ -11,7 +11,19 @@ g1([person(kara, [barry, clark]),
  person(clark, [oliver, kara]),
  person(oliver, [kara])]).
 
-/* */
+g2([person(batman, [green_arrow, superman]),
+    person(green_arrow, [supergirl]),
+    person(supergirl, [flash, superman]),
+    person(flash, [green_arrow, supergirl]),
+    person(superman, [green_arrow, supergirl])]).
+
+a([p(kara,supergirl), 
+    p(bruce,batman), 
+    p(barry,flash), 
+    p(clark,superman), 
+    p(oliver,green_arrow)]).
+
+% Task a
 likes(G,X,Y) :- 
     isMember(person(X, Friends), G),
     isMember(Y, Friends).
@@ -20,7 +32,7 @@ likes(G,X,Y) :-
 isMember(Head,[Head|_]).
 isMember(Head,[_|Tail]) :- isMember(Head,Tail).
 
-/* b - using not which may not be allowed */
+% Task b
 dislikes(G, X, Y) :-
     different(G, X, Y),
     likes(G, Y, X),
@@ -31,13 +43,18 @@ different(G, X, Y) :-
     selectList(person(X, _), G, G2),
     selectList(person(Y, _), G2, _).
 
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+select?
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
 selectList(X, [X|Tail], Tail).
 selectList(Elem, [Head|Tail], [Head|Rest]) :-
     select(Elem, Tail, Rest).
 
-
-notLikes(G, X, Y) :- getFriends(G, X, Xfriends),
-        isNotFriend(G, Y, Xfriends).
+notLikes(G, X, Y) :- 
+    getFriends(G, X, Xfriends),
+    isNotFriend(G, Y, Xfriends).
 
 isNotFriend(_, _, []).
 isNotFriend(G, X, [Friend|RestFriends]) :- 
@@ -47,8 +64,8 @@ isNotFriend(G, X, [Friend|RestFriends]) :-
 getFriends([person(X, Friends)|_], X, Friends).
 getFriends([_|T], X, Friends) :- getFriends(T, X, Friends).            
 
-
-% Level 1 - Task c
+% Level 1
+% Task c
 popular(G, X) :-
     isMember(person(X, Friends), G),
     allLikingX(G, X, Friends).
@@ -74,12 +91,15 @@ friendly(G, X) :-
     checkFriendliness(G, G, X).
 
 checkFriendliness([], _, _).
+% skip where name is same as X
 checkFriendliness([person(Name, _)|Tail], G, Name) :-
     checkFriendliness(Tail, G, Name).
+% check for like in both directions
 checkFriendliness([person(Name, _)|Tail], G, X) :-
     likes(G, Name, X),
     likes(G, X, Name),
     checkFriendliness(Tail, G, X).
+% check that there is no friend(likes) relation
 checkFriendliness([person(_, Friends)|Tail], G, X) :-
     isNotFriend(G, X, Friends),
     checkFriendliness(Tail, G, X).
@@ -90,12 +110,37 @@ hostile(G, X) :-
     checkHostileness(G, G, X).
 
 checkHostileness([], _, _).
+% skip where name is same as X
 checkHostileness([person(Name, _)|Tail], G, Name) :-
     checkHostileness(Tail, G, Name).
+% check for like in one direction and dislike in the other
 checkHostileness([person(Name, _)|Tail], G, X) :-
     likes(G, Name, X),
     dislikes(G, X, Name),
     checkHostileness(Tail, G, X).
+% check that there is no friend(likes) relation
 checkHostileness([person(_, Friends)|Tail], G, X) :-
     isNotFriend(G, X, Friends),
     checkHostileness(Tail, G, X).
+
+% Level 2
+% Task g
+admires(G, X, Y) :-
+    different(G, X, Y),
+    checkAdmires(G, X, Y, [X, Y]).
+
+% check first for direct like, else check like-chain with list of all likes
+checkAdmires(G, X, Y, _) :- likes(G, X, Y).
+checkAdmires(G, X, Y, List) :-
+    % notLikes(G, X, Y),
+    likes(G, X, Z),
+    isNotFriend(G, Z, List), % checks that there is no loop
+    checkAdmires(G, Z, Y, [Z|List]).
+    
+% Task h
+% indifferent(G, X, Y) :-
+
+
+% Level 3
+% Task i
+% same_world(G, H, A) :-
