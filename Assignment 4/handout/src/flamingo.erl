b133fe63763caf2949827315d07fe3cf250cc3b5
Kai Arne Myklebust <kai25arne@icloud.com>
2018-10-09 12:04:19 +0200

    Flamingo groups

--- Assignment 4/handout/src/flamingo.erl
+++ Assignment 4/handout/src/flamingo.erl
@@ -3,7 +3,7 @@
 -export([new/1, request/4, route/4, drop_group/2]).
 
 new(Global) ->
-  try ({ok, spawn(fun() -> loop(Global, #{}) end)})
+  try ({ok, spawn(fun() -> loop(Global, []) end)})
   catch
     _:Error -> {error, Error}
   end.
@@ -21,18 +21,34 @@ route(Flamingo, Path, Fun, Arg) ->
 drop_group(_Flamingo, _Id) ->
   not_implemented.
 
-loop(Global, Routes) ->
+loop(Global, RouteGroup) ->
   receive
     {From, request, {Path, Request}, Ref} ->
-      case maps:is_key(Path, Routes) of
-        true -> F = maps:get(Path, Routes),
+      case maps:is_key(Path, RouteGroup) of
+        true -> F = maps:get(Path, RouteGroup),
           Content  = F({Path, Request}, Global, Ref),
           From ! {Ref, {200, Content}}
       end,
-      loop(Global, Routes);
+      loop(Global, RouteGroup);
     {From, routes, Path, Fun, Arg} ->
-      L = [{X, Fun} || X <- Path],
-      NewRoutes = maps:merge(Routes, maps:from_list(L)),
+      NewRoutes = updateRouteGroups(Path, Fun, Arg, RouteGroup),
       From ! self(),
       loop(Global, NewRoutes)
   end.
+
+% RouteGroup = {[Paths], Fun, Arg}
+
+updateRouteGroups(Path, Fun, Arg, OldGroup) ->
+  [{Path, Fun, Arg} | updateOldGroup(Path, OldGroup)].
+
+updateOldGroup(NewPath, []) -> [];
+updateOldGroup(NewPath, [{Path, Fun, Arg} | GroupTail]) ->
+  [{updateOldGroupPaths(NewPath, Path), Fun, Arg} | updateOldGroup(NewPath, GroupTail)].
+
+updateOldGroupPaths(NewPath, []) -> [];
+updateOldGroupPaths(NewPath, [OldPath | OldPathTail]) -> 
+  case lists:member(OldPath, NewPath) of
+    true -> updateOldGroupPaths(NewPath, OldPathTail);
+    false -> [OldPath | updateOldGroupPaths(NewPath, OldPathTail)]
+  end.
+  
