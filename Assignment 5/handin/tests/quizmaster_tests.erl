18d4e25ccaaa4cf44d3e03976a9d297751be0fdc
Silvan Adrian <hallo@silvanadrian.ch>
2018-10-18 17:48:40 +0200

    getting sick of typesetting manually :zap::zap:

--- Assignment 5/handin/tests/quizmaster_tests.erl
+++ Assignment 5/handin/tests/quizmaster_tests.erl
@@ -3,10 +3,17 @@
 
 do_you_want_to_play_a_game() ->
   {ok, Q} = quizmaster:start(),
+
   quizmaster:add_question(Q, {"Answer to the Ultimate Question of Life, the Universe, and Everything?", ["43", "34", {correct, "42"}, "24"]}),
   quizmaster:add_question(Q, {"How many i's are in Ken's name?", [{correct, "2"}, "10", {correct, "0"}, "6"]}),
   quizmaster:add_question(Q, {"How many legs does a Flamingo have?", ["1", "2", {correct, "0"}, "4"]}),
-  quizmaster:get_questions(Q),
+
+  C = quizmaster_conductor:start(),
+  quizmaster_conductor:play(C, Q),
+
+  Player1 = quizmaster_player:start(),
+  Player2 = quizmaster_player:start(),
+
   quizmaster:join(Q, "Nick"), % won't be able to join
   quizmaster:play(Q),
   {ok, Ref} = quizmaster:join(Q, "Nick"),
