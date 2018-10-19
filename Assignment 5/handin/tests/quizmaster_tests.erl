2236c5b273b8a2deca76f4f70e2a1d5bdb3dd47d
Silvan Adrian <hallo@silvanadrian.ch>
2018-10-19 14:36:50 +0200

    first finished version

--- Assignment 5/handin/tests/quizmaster_tests.erl
+++ Assignment 5/handin/tests/quizmaster_tests.erl
@@ -1,24 +1,6 @@
 -module(quizmaster_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-%%do_you_want_to_play_a_game() ->
-%%  {ok, Q} = quizmaster:start(),
-%%
-%%  quizmaster:add_question(Q, {"Answer to the Ultimate Question of Life, the Universe, and Everything?", ["43", "34", {correct, "42"}, "24"]}),
-%%  quizmaster:add_question(Q, {"How many i's are in Ken's name?", [{correct, "2"}, "10", {correct, "0"}, "6"]}),
-%%  quizmaster:add_question(Q, {"How many legs does a Flamingo have?", ["1", "2", {correct, "0"}, "4"]}),
-%%
-%%  C = quizmaster_conductor:start(),
-%%  quizmaster_conductor:play(C, Q),
-%%
-%%  Player1 = quizmaster_player:start(),
-%%  Player2 = quizmaster_player:start(),
-%%
-%%  P = quizmaster_player:join(Player1, Q, "Jigsaw"),
-%%  %%quizmaster_player:join(Player2, Q, "Jason Voorhees"),
-%%  %%timer:sleep(1000),
-%%  P.
-
 quizmaster_start_test() ->
   ?assertMatch({ok, _}, quizmaster_server()).
 
